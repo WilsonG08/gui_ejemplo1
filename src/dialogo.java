5611334573f11b38096c393e899d1063e47adc9f
Wilson <wilson.guayanay@epn.edu.ec>
2023-01-16 11:09:48 -0500

    Initial commit

---
+++ src/dialogo.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+public class dialogo {
+    public static void main(String[] args) {
+        //JOptionPane.showMessageDialog(null,"Bienvenio\na\nJAVA");
+        //JOptionPane.showMessageDialog(null,"Hola Mundo");
+
+
+        String nombre=
+                JOptionPane.showInputDialog("¿Cuál es su nombre?:");
+        String mensaj=
+                String.format("Bienbenido, %s,  a la progrmcion en Java!",nombre);
+
+        JOptionPane.showMessageDialog(null,mensaj);
+
+
+
+    }
+}
